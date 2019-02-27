786760d758a24b360b8ae53afe05fd453094671e
Nicofh <598220654@qq.com>
2019-02-27 17:34:28 +0800

    增加spring security

---
+++ wx/src/main/java/com/fuhua/wx/security/MyUserDetailsService.java
@@ -0,0 +1,42 @@
+package com.fuhua.wx.security;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class MyUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        log.info(username);
+
+        // TODO 根据用户名，查找到对应的密码，与权限
+
+        // 封装用户信息，并返回。参数分别是：用户名，密码，用户权限
+
+        // 对密码进行加密，这里用了BCryptPasswordEncoder加密。一般数据库都加密了MD5加密，
+        String password = passwordEncoder.encode("123456");
+
+        log.info("密码是：{}", password);
+
+        // User 是 userdetails 的
+
+        User user = new User(username, password, AuthorityUtils.commaSeparatedStringToAuthorityList("admin"));
+
+        return user;
+    }
+
+
+}
